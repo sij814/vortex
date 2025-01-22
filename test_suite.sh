86418a45ac4ef50eb2ddc77dfa96ab5081fbfb9a
sij814 <sij814@cs.ucla.edu>
2025-01-22 14:55:27 -0800

    minor change to test_suite

--- test_suite.sh
+++ test_suite.sh
@@ -1,13 +1,13 @@
 cd build
+source ./ci/toolchain_env.sh
 
 rm output.txt
 
 make -s
 
-./ci/blackbox.sh --driver=simx --app=blackscholes --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --rebuild=1 --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=conv3 --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=saxpy --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=sgemm --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=sgemm3 --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=stencil3d --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
-./ci/blackbox.sh --driver=simx --app=vecaddx --args='-n16384' --cores=8 --clusters=8 --l2cache --l3cache --perf=2 >> output.txt
+./ci/blackbox.sh --driver=simx --app=blackscholes --args='-n64' --cores=8 --clusters=4 --l2cache --l3cache --rebuild=1 --perf=2 >> output.txt #2D
+./ci/blackbox.sh --driver=simx --app=conv3 --args='-n64' --cores=8 --clusters=4 --l2cache --l3cache  --perf=2 >> output.txt #2D
+./ci/blackbox.sh --driver=simx --app=saxpy --args='-n4096' --cores=8 --clusters=4 --l2cache --l3cache  --perf=2 >> output.txt #1D
+./ci/blackbox.sh --driver=simx --app=sgemmx --args='-n64' --cores=8 --clusters=4 --l2cache --l3cache --perf=2 >> output.txt #2D
+./ci/blackbox.sh --driver=simx --app=sgemm2x --args='-n64' --cores=8 --clusters=4 --l2cache --l3cache --perf=2 >> output.txt #2D
+./ci/blackbox.sh --driver=simx --app=vecaddx --args='-n4096' --cores=8 --clusters=4 --l2cache --l3cache --perf=2 >> output.txt #1D
